df0e233223430d7f66e84106e7e2a8b68ef8e0bb
pr7nce <91542922+pr7nce@users.noreply.github.com>
2022-01-29 16:05:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb+srv://its-me:mario@cluster0.76uuu.mongodb.net/todolistDB", {useNewUrlParser:true});
+mongoose.connect("/todolistDB", {useNewUrlParser:true});
 
 
 const itemsSchema={
